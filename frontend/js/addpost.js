c1e4090b984267e1297163465b6ed253db5a3c9e
Andrea G <andreagrasso.ag@live.it>
2020-08-01 18:19:28 +0200

    text add img

--- frontend/js/addpost.js
+++ frontend/js/addpost.js
@@ -10,13 +10,15 @@ messageForm.addEventListener('submit', function (e) {
     e.preventDefault()
     const title = e.target.title.value
     const content = e.target.post.value
+    const image = e.target.img.value //***********************************//
 
 
-    axios.post('http://localhost:3000/api/messages/new', { title, content }, tokenKey)
+    axios.post('http://localhost:3000/api/messages/new', { title, content, image }, tokenKey)
         .then((resp) => {
             window.location.href = 'index.html'
 
         })
+
 });
 
 
